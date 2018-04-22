fd951ef95bc4f3c8ce13925e88253fdf3eba9957
root <root@localhost.localdomain>
2018-04-22 19:31:04 -0400

    fixed ssh remaining bytes error

--- main/test/ssh-pycryptodome.py
+++ main/test/ssh-pycryptodome.py
@@ -6,25 +6,25 @@ import binascii
 recipient_key = RSA.generate(2048, e=17)
 session_key = get_random_bytes(16)
 
-# Encrypt the session key with the public RSA key
-message = "Hello, world!"
-# data = message.encode()
-cipher_rsa = PKCS1_OAEP.new(recipient_key)
-ciphertext_rsa = cipher_rsa.encrypt(message)
-print "len: " + str(len(ciphertext_rsa))
-print repr(ciphertext_rsa)
+# # Encrypt the session key with the public RSA key
+# message = "Hello, world!"
+# # data = message.encode()
+# cipher_rsa = PKCS1_OAEP.new(recipient_key)
+# ciphertext_rsa = cipher_rsa.encrypt(message)
+# print "len: " + str(len(ciphertext_rsa))
+# print repr(ciphertext_rsa)
 
 # Encrypt the data with the AES session key
 # cipher_aes = AES.new(session_key, AES.MODE_EAX)
 # ciphertext, tag = cipher_aes.encrypt_and_digest(data)
 #[ file_out.write(x) for x in (cipher_aes.nonce, tag, ciphertext) ]
 
-# print "e:"
-# print recipient_key.e
-# print "n:"
-# print recipient_key.n
-# print "d:"
-# print recipient_key.d
+print "e:"
+print recipient_key.e
+print "n:"
+print recipient_key.n
+print "d:"
+print recipient_key.d
 
 # rsa_key = {
 # 	"e": 17,
